3c3654cf4fefdcfd7d33ba469a358b1fca3360fe
alfathmuqoddas <alfath.muqoddas@gmail.com>
2021-09-19 22:03:24 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include 'templates/header.php' ?>
+
+<div class="py-20">
+	<div class="container mx-auto px-4 md:px-20">
+		<h1 class="text-5xl">This works I guest</h1>
+		<div class="pt-4">
+			<?php include 'server-connect.php' ?>
+		</div>
+	</div>
+	
+</div>
+
+
+<?php include 'templates/footer.php' ?>
---
+++ templates/footer.php
@@ -0,0 +1,7 @@
+    <footer class="py-20 bg-green-50">
+		<div class="container mx-auto px-4 md:px-20 text-center">
+			Copyright Alfath Muqoddas ©2021
+		</div>
+    </footer>
+  </body>
+</html>

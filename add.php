22c5d5547914a83e9f17295b92aaa2eed938c3f5
alfathmuqoddas <alfath.muqoddas@gmail.com>
2021-09-22 00:06:51 +0700

    adding validation and error

--- add.php
+++ add.php
@@ -1,28 +1,38 @@
 <?php include 'templates/header.php' ?>		
 
 <?php
+  $errors = array('email' => '', 'title' => '', 'post' => '' );
 
   if (isset($_POST['submit'])) {
 
-  	// check title
-  	if (empty($_POST['title'])) {
-  		echo "Please enter title";
-  	} else {
-  		echo htmlspecialchars($_POST['title']);
-  	}
-
   	// check email
   	if (empty($_POST['email'])) {
-  		echo "An email is required";
+  		$errors['email'] = "Please enter email <br />"; 
   	} else {
-  		echo htmlspecialchars($_POST['email']);
+  		$email = $_POST['email'];
+  		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  			$errors['email'] = 'email must be valid';
+  		}
+  	}
+
+  	// check title
+  	if (empty($_POST['title'])) {
+  		$errors['title'] = "An title is required <br />";
+  	} else {
+  		$title = $_POST['title'];
+  		if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+  			$errors['title'] = 'Title must only contain text and spaces';
+  		}
   	}
 
   	// check post
   	if (empty($_POST['post'])) {
-  		echo "Enter your post please";
+  		$errors['post'] = "Enter your post please <br />";
   	} else {
-  		echo htmlspecialchars($_POST['post']);
+  		$post = $_POST['post'];
+  		if (!preg_match('/^[a-zA-Z\s]+$/', $post)) {
+  			$errors['post'] = 'Post must be comma separated list';
+  		}
   	}
   }
 
@@ -34,13 +44,16 @@
     <form action="add.php" method="post" class="bg-blue-100 p-8 rounded-md w-4/12 mx-auto">
 		  <label for="title">Title</label>
 		  <br>
-		  <input class="p-4 border border-blue-100 rounded-full w-full mb-4" type="text" name="title" placeholder="enter title" />
+		  <input class="p-4 border border-blue-100 rounded-full w-full" type="text" name="title" placeholder="enter title" />
+		  <div class="text-red-500 mb-4"><?php echo $errors['title']; ?></div>
 		  <label for="email">Email</label>
 		  <br>
-		  <input class="p-4 border border-blue-100 rounded-full w-full mb-4" type="email" name="email" placeholder="enter email" />
+		  <input class="p-4 border border-blue-100 rounded-full w-full" type="email" name="email" placeholder="enter email" />
+		  <div class="text-red-500 mb-4"><?php echo $errors['email']; ?></div>
 		  <label for="post">Post</label>
 		  <br>
 		  <input class="p-4 border border-blue-100 rounded-full w-full" type="text" name="post" placeholder="enter post here"/>
+		  <div class="text-red-500 mb-4"><?php echo $errors['post']; ?></div>
 		  <input type="submit" name="submit" class="p-2 w-full rounded-full bg-blue-500 hover:bg-blue-400 cursor-pointer mt-4">
 		</form>
   </div>
